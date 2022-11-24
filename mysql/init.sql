98526abd65fa57c7a1365b857c01c97a785b08ed
Merlin <merlin-1@o2.pl>
2022-11-24 20:14:23 +0100

    DB update

--- mysql/init.sql
+++ mysql/init.sql
@@ -5,15 +5,43 @@ FLUSH PRIVILEGES;
 CREATE DATABASE app;
 USE app;
 
-CREATE TABLE post(
+CREATE TABLE Post(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     author VARCHAR(20),
     content TEXT
 );
 
-INSERT INTO post
-    VALUES
-    (null,"Elon Musk", "The CyberTruck is here! #CyberTruck"),
-    (null,"Tory Bruno", "ULA will be launching atlas V this week, get your popcorn ready #ULA #AtlasV #Space"),
-    (null,"Freya Homer","SPLINES ARE THE HORROR!!!");
-    
+
+CREATE TABLE Image(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    image_data VARBINARY(MAX) NOT NULL
+);
+
+CREATE TABLE Comment(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(255),
+    creation_date DATE NOT NULL default current_timestamp ON UPDATE current_timestamp 
+
+);
+
+CREATE TABLE Post(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    content VARCHAR(999) NOT NULL,
+    image INT FOREIGN KEY REFERENCES Image(id),
+    creation_date DATE NOT NULL default current_timestamp,
+    comments INT FOREIGN KEY REFERENCES Comment(id)
+);
+
+CREATE TABLE Profile(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL,
+    profile_picture INT FOREIGN KEY REFERENCES Image(id) NOT NULL,
+);
+
+CREATE TABLE User(
+    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(20) NOT NULL, 
+    password CHAR(40) NOT NULL, -- hashed with SHA-1
+    profile INT FOREIGN KEY REFERENCES Profile(id) NOT NULL,
+);
+
