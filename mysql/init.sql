ce1916bca67f1dbaa5d0be400d49d8bae53138e0
Merlin <merlin-1@o2.pl>
2022-12-13 16:49:22 +0100

    database_update

--- mysql/init.sql
+++ mysql/init.sql
@@ -7,6 +7,7 @@ USE app;
 
 CREATE TABLE Post(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    
     author VARCHAR(20),
     content TEXT
 );
@@ -14,44 +15,64 @@ CREATE TABLE Post(
 
 CREATE TABLE Image(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    
     image_data VARBINARY(MAX) NOT NULL
 );
 
 CREATE TABLE Comment(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    content VARCHAR(255),
-    creation_date DATE NOT NULL default current_timestamp ON UPDATE current_timestamp 
 
+    post_id INT,
+    content VARCHAR(255),
+    creation_date DATE NOT NULL default current_timestamp ON UPDATE current_timestamp, -- update automatic data on create and update 
+
+    FOREIGN KEY post_id REFERENCES Post(id)
 );
 
 CREATE TABLE Post(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    
     content VARCHAR(999) NOT NULL,
-    image INT FOREIGN KEY REFERENCES Image(id),
+    image_id INT,
     creation_date DATE NOT NULL default current_timestamp,
-    comments INT FOREIGN KEY REFERENCES Comment(id)
+
+    FOREIGN KEY image_id REFERENCES Image(id)
+
 );
 
 CREATE TABLE Profile(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(20) NOT NULL,
-    profile_picture INT FOREIGN KEY REFERENCES Image(id) NOT NULL,
-    description VARCHAR(100) NOT NULL,
     
+    username VARCHAR(20) NOT NULL,
+    profile_picture_id INT NOT NULL,
+    profile_desc VARCHAR(100) NOT NULL,
+    
+    FOREIGN KEY profile_picture_id REFERENCES Image(id)
 );
 
 CREATE TABLE Follow(
-    follower INT,
-    followed INT,
-    PRIMARY KEY, 
-    
+    follower_id INT,
+    followed_id INT,
+
+    FOREIGN KEY follower_id REFERENCES Profile(id),
+    FOREIGN KEY followed_id REFERENCES Profile(id),
+    CONSTRAINT id PRIMARY KEY (follower_id,followed_id),
 );
 
 CREATE TABLE User(
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    
     username VARCHAR(20) NOT NULL, 
     password CHAR(40) NOT NULL, -- hashed with SHA-1
-    profile INT FOREIGN KEY REFERENCES Profile(id) NOT NULL,
+    profile_id INT NOT NULL,
+
+    FOREIGN KEY profile_id REFERENCES Profile(id)
 );
 
+CREATE TALBE LIKE(
+    profile_id INT,
+    post_id INT,
 
+    FOREIGN KEY profile_id REFERENCES Profile(id),
+    FOREIGN KEY post_id REFERENCES Post(id)
+)
