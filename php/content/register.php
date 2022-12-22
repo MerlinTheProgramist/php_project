2f042a241b1cd36ddfd0c53570609920c961481f
Merlin <merlin-1@o2.pl>
2022-12-22 17:52:08 +0100

    Login & register

--- php/content/register.php
+++ php/content/register.php
@@ -13,7 +13,7 @@
         <title>Twitter2 Login</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="./css/style.css" type=>
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
         <div id="main">
@@ -23,13 +23,15 @@
             <?php unset($_SESSION["mess"]); endif;?>
             
             <form method="POST" action="verify.php">
+                <label>e-mail: </br><input type="email" name="email"></input></label></br>    
                 <label>Login: </br><input type="text" name="login"></input></label></br>
                 <label>Password: </br><input type="password" name="pass"></input></label></br>
-                <input type="submit" name="sub" value="Login"></input></br>
+                <label>Repeat password: </br><input type="password" name="pass1"></input></label></br>
+                <input type="submit" name="sub" value="Next"></input></br>
             </form>
 
-            <label>Nie masz jeszcze konta? Zarejestruj się 
-                <a href="register.php">tutaj</a>
+            <label>Masz już konto? Zaloguj się 
+                <a href="login.php">tutaj</a>
             </label>
         </div>
     </body>
