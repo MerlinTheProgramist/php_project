bfd3439846397752e2f2abec816bff54d3f1a1da
Merlin <merlin-1@o2.pl>
2022-12-27 16:12:27 +0100

    finally auth works

--- php/content/verify.php
+++ php/content/verify.php
@@ -17,14 +17,17 @@ $pass = sha1($_POST["pass"]);
 if ($_POST["sub"] == "Login") {
     echo "Loging";
     $db = mysqli_connect("db", "mysql_user", "mysql_pass", "app",3306);
-    $result = mysqli_query($db, "SELECT u.login, i.image_path FROM User u JOIN Profile p ON u.profile_id=p.id JOIN Image i ON i.id=p.profile_picture_id WHERE u.password='{$pass}' AND (u.login='{$login}' OR u.email='{$login}');");
-    $data;
-    if(!($data = mysqli_fetch_array($result)))
+    $result = mysqli_query($db, "SELECT u.username, i.image_path, u.id FROM User u JOIN Profile p ON u.profile_id=p.id JOIN Image i ON i.id=p.profile_picture_id WHERE u.password='{$pass}' AND (u.username='{$login}' OR u.email='{$login}');");
+    $data = mysqli_fetch_array($result);
+    if(!$data)
         set_n_head("./login.php", array('mess'=>"login or password was incorrect"));
     
+    $_SESSION["user_id"] = $data["id"];
     $_SESSION["auth"] = true;
     $_SESSION["login"] = $data["login"];
     $_SESSION["profile_pic"] = $data["image_path"];
+
+    header("Location: ./home.php");
 } 
 else if ($_POST["sub"] == "Next") {
     echo "Singing in";
@@ -36,19 +39,38 @@ else if ($_POST["sub"] == "Next") {
 
     $db = mysqli_connect("db", "mysql_user", "mysql_pass", "app", 3306);
     // create account
-    mysqli_query($db, "INSERT INTO User(email,username,password) VALUES ('{$email}', '{$login}','{$pass}');");
-    // get default picture
-    $result = mysqli_query($db, "SELECT u.login, i.image_path FROM User u JOIN Profile p ON u.profile_id=p.id JOIN Image i ON i.id=p.profile_picture_id WHERE u.password='{$pass}' AND u.email='{$email}');");
+    
+    // check if user already created
+    $email_present = mysqli_query($db,"SELECT count(1) FROM User u WHERE u.email='{$email}'");
+    if (mysqli_fetch_array($email_present)[0]>0) // user with this email already exists
+        set_n_head("./register.php", array('mess'=>"There is already an account with this email"));
+    $username_present = mysqli_query($db,"SELECT count(1) FROM User u WHERE u.username='{$login}'");
+    if (mysqli_fetch_array($username_present)[0]>0) // user with this name already exists
+        set_n_head("./register.php", array('mess'=>"Login is Occupied!</br> Please insert other Login"));
+    
+    
+    CreateUser($db, $email, $login, $pass);
 
+    // get default picture
+    $result = mysqli_query($db, "SELECT u.username, i.image_path, p.id FROM User u 
+                                    LEFT JOIN Profile p ON u.profile_id=p.id 
+                                    LEFT JOIN Image i ON i.id=p.profile_picture_id 
+                                    WHERE u.password='{$pass}' AND u.email='{$email}';");
+    if (!$result)
+        set_n_head("./register.php", array('mess' => $result));
     $data = mysqli_fetch_array($result);
+    
     $_SESSION["auth"] = true;
+    $_SESSION["user_id"] = $data["id"];
     $_SESSION["login"] = $data["login"];
     $_SESSION["profile_pic"] = $data["image_path"];
+
+    header("Location: ./home.php");
 }
 else{
     set_n_head("./login.php",array('mess'=>"404"));
 }
 
-header("Location: ./home.php");
+
 
 ?>
