1bf36c5c4a168cea2a16e3fa47f3e6fc6b383d7b
Merlin <merlin-1@o2.pl>
2022-12-29 15:26:26 +0100

    util

--- php/content/util.php
+++ php/content/util.php
@@ -51,4 +51,75 @@ function CreateUser($db,string $email, string $name, string $pass)
                       ('{$email}','{$name}','{$pass}',LAST_INSERT_ID());");
 }
 
+function userTemplate($db, int $id, string $image_path, string $username, string $desc, bool $following)
+{
+    ?>
+    <div class="profile">
+        <img class="avatar" src="<?=('./profile_pics/'.$image_path)?>">
+        <div class="meta">
+            
+            <b><?=$username?></b>
+            <button type="submit" class="follow"
+            value=<?=($following)?"1":"0"?>>
+            </button>
+        </div>
+        <p><?=$desc?></p>
+        <hr>
+    </div> 
+    <?php
+}
+
+function postTemplate($db, int $id, ?string $image_path, string $author,string $creation_time, string $text){
+    ?>
+    <div class="post">
+        <div class="meta">
+            <a class="author">
+                <img class="avatar" src="<?=('./profile_pics/'.$image_path)?>">
+                <?=$author?>
+                ·
+                <small><?=$creation_time?></small>
+            </a> 
+            
+        </div>
+        <p>
+            <?=$text?>
+        </p>
+        <hr>
+        <?php
+        $url = urlGET('/post.php',array('id'=>$id));
+        $is_hearting = mysqli_fetch_array(mysqli_query($db, "SELECT count(*) FROM Heart h WHERE {$_SESSION['user_id']}=h.profile_id AND h.post_id={$id};"))[0];
+        if (isset($_POST['Heart']) && $_POST['Heart']==$id) {
+            $h_id = $_POST['Heart'];
+        
+            if($is_hearting) // unheart
+                mysqli_query($db, "DELETE FROM Heart h WHERE h.profile_id={$_SESSION['user_id']} AND h.post_id={$h_id};");
+            else // heart
+                mysqli_query($db, "INSERT INTO Heart (profile_id, post_id) VALUES ({$_SESSION['user_id']},{$h_id});");
+            $is_hearting = !$is_hearting;
+
+        }
+        $hearths = mysqli_fetch_array(mysqli_query($db, "SELECT count(1) as num FROM Heart h WHERE h.post_id={$id};"));            
+        ?>
+        <table class="actions">
+            <td>
+                <a href="<?=$url?>">
+                Comm
+                </a>
+            </td>
+            <td>
+                <h3><?=$hearths['num']?></h3>
+                <form method='POST'>
+                    <button onclick="" type="submit" name="Heart" value="<?=$post['id']?>" class="heart" <?=(($is_hearting)?"fill":"")?>></button>
+                </form>
+            </td>
+            <td>
+                <button onclick="copyText('<?=$url?>')">
+                Share <!-- copy link to clickboard -->
+                </button>
+            </td>
+        </table>
+    </div>
+    <?php
+}
+
 ?>
