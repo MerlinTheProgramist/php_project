c6f3203f65be625e22e71b6513834d068662bf26
Merlin <merlin-1@o2.pl>
2023-01-01 21:01:05 +0100

    Comments

--- php/content/post.php
+++ php/content/post.php
@@ -1,8 +1,31 @@
 <?php
 session_start();
 
-if (!isset($_SESSION["login"]) || $_SESSION["login"] == "")
-    header("index.html");
+if (!isset($_SESSION["user_id"]))
+header("Location: login.php");
+
+require("util.php");
+
+
+$id = $_GET['id'];
+
+$db = mysqli_connect("db", "mysql_user", "mysql_pass", "app",3306);
+$result = mysqli_query($db, 
+    "SELECT prof.username as author,
+    i.image_path as prof,
+    p.content as cont,
+    p.creation_date as cre_time 
+    FROM Post p
+    LEFT JOIN Profile prof ON prof.id=p.author_id
+    LEFT JOIN Image i ON prof.profile_picture_id=i.id
+    WHERE p.id={$id}
+    LIMIT 1 
+");
+$post = mysqli_fetch_array($result);
+if (!$post) {
+    header("Location: home.php");
+    die();
+}
 ?>
 
 <!DOCTYPE html>
@@ -23,50 +46,68 @@ if (!isset($_SESSION["login"]) || $_SESSION["login"] == "")
         <link rel="stylesheet" href="./css/style.css">
     </head>
 
-    <?php
-        $db = mysqli_connect("db", "mysql_user", "mysql_pass", "app",3306);
-        $result = mysqli_query($db, "SELECT prof.username as author, i.image_path as prof,
-            p.content as cont, p.creation_date as cre_time FROM Post p 
-            LEFT JOIN Profile prof ON prof.id=p.author_id 
-            LEFT JOIN Image i ON i.id=p.image_id ORDER BY p.creation_date;");    
-    ?>
-
     <body>
         <div id="main">
         <?php include "./sidenav.html" ?>
-
-        <?php
-        $id = $_GET['id'];
-
-        $db = mysqli_connect("db", "mysql_user", "mysql_pass", "app",3306);
-        $result = mysqli_query($db, 
-           "SELECT prof.username as author,
-            i.image_path as prof,
-            p.content as cont,
-            p.creation_date as cre_time 
-            FROM Post p
-            LEFT JOIN Profile prof ON prof.id=p.author_id
-            LEFT JOIN Image i ON prof.profile_picture_id=i.id
-            WHERE p.id={$id}
-            LIMIT 1 
-        ");
-        $post = mysqli_fetch_array($result);
-        ?>
         
         <div class="post">
-                    <div class="meta">
-                        <a class="author">
-                            <img class="prof_pic" src="<?=('./profile_pics/'.$post['prof'])?>">
-                            <?=$post["author"]?>
-                            ·
-                            <small><?=$post["cre_time"]?></small>
-                        </a> 
-                        
-                    </div>
-                    <p>
-                        <?=$post["cont"]?>
-                    </p>
-                </div>
+            <div class="meta">
+                <a class="author">
+                    <img class="prof_pic" src="<?=('./profile_pics/'.$post['prof'])?>">
+                    <?=$post["author"]?>
+                    ·
+                    <small><?=$post["cre_time"]?></small>
+                </a> 
+                
+            </div>
+            <p>
+                <?=nl2br($post["cont"])?>
+            </p>
+        </div>
+
+        
+
+        <div id="commets">
+
+            <!-- New comment -->
+            <form method="POST" action="<?=urlGET('./post.php',array('id'=>$_GET['id']))?>">
+                <textarea type="text"  name="content"></textarea>
+                <button type="submit" name="sub">Comment</button>
+            </form>
+            <?php
+                if(isset($_POST["sub"]))
+                {
+                    $text = $_POST['content'];
+                if (!empty($text))
+                    mysqli_query($db, "INSERT INTO Comment (author_id, post_id, content) values({$_SESSION['user_id']},{$id},'{$text}');");
+                }
+            ?>
+
+            <!-- Comments and deletion -->
+            <?php
+            if (isset($_POST['delete']))
+                mysqli_query($db, "DELETE FROM Comment c WHERE c.id={$_POST['delete']};");
+
+            $result = mysqli_query($db, "SELECT c.content as cont,
+                                         c.id as id,
+                                         prof.id as author_id,
+                                         prof.username as author,
+                                         prof_img.image_path as prof_pic,
+                                         c.creation_date as cre_time
+                                         FROM Comment c
+                                         JOIN Profile prof ON prof.id=c.author_id
+                                         JOIN Image prof_img ON prof.profile_picture_id=prof_img.id
+                                         WHERE c.post_id={$id};");
+            while ($comm = mysqli_fetch_array($result)) {
+                commentTemplate($comm['author'], $comm['prof_pic'], $comm['author_id'], $comm['cre_time'], $comm['cont'], $comm['id'], $id);
+            }
+            ?>
+        </div>
+        <script>
+            if ( window.history.replaceState ) {
+            window.history.replaceState( null, null, window.location.href );
+            }
+        </script>
         </div>
     </body>
 </html>
