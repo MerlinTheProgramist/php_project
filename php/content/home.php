42c9c0b4ea52b6a992e0bd529fe9aeeeaae153df
Merlin <merlin-1@o2.pl>
2023-02-16 21:35:00 +0100

    Update home.php

--- php/content/home.php
+++ php/content/home.php
@@ -11,7 +11,7 @@ require("util.php");
 <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
 <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-<!--[if gt IE 8]>      <html class="no-js"> <!--<![endif]-->
+<!--[if gt IE 8]>      <html class="no-js"> <![endif]-->
 
 
 
