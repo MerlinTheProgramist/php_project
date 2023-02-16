f9ec34e05a6159ff493ddc8bb720f33e1198a0fc
Merlin <merlin-1@o2.pl>
2023-02-16 21:35:18 +0100

    auto head to home

--- php/content/index.php
+++ php/content/index.php
@@ -1,43 +1,4 @@
-<!doctype html>
-<html lng="en">
-<head>
-    <meta charset="utf-8">
-    <title>Docker Test</title>
-    <meta name="description" content="Docker Test">
-    <meta name="author" content="Merlin">
-</head>
-<!-- STRONA GŁÓWNA, tak ta której miało nie być-->
-<body>
-    <h1>THIS IS A TEST, nothing to see here</h1>
-    <div class=".db-table">
-        <table>
-            <tr>
-                <th>Id</th>
-                <th>Message</th>
-            </tr>
-            <?php
-                $user = 'mysql_user';
-                $pass = 'mysql_pass';
-
-                try{
-                    $dbh = new PDO('mysql:host=db;port=3306;dbname=app', $user, $pass);
-                    foreach ($dbh->query('SELECT * from post') as $post):?>        
-                    <div class="post">
-                        <h2>By: <?= $post["author"]  ?></h2>
-                        <quote>
-                            <?= $post["content"] ?>  
-                        </quote>
-                    </div>
-                    <?php
-                    endforeach;
-                    $dbh = null;
-                } catch (PDOException $e) {
-                    print "Error!: " . $e->getMessage() . "<br/>";
-                    die();
-                }
-            ?>
-        </table>
-    </div>
-</body>
-
-</html>
+<?php
+    header('Location: home.php');
+    exit();
+?>
